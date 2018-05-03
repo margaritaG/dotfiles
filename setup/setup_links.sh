471880baf0d2dffc104b43802ebd31c6170f0b5d
Margarita Grinvald <margaritagrinvald@gmail.com>
2018-05-03 16:49:36 +0200

    Add custom theme

--- setup/setup_links.sh
+++ setup/setup_links.sh
@@ -5,3 +5,4 @@ DOTFILES_PATH="/home/${USER}/dotfiles"
 
 # zshrc
 ln -sf "$DOTFILES_PATH/zsh/zshrc" "$HOME_FOLDER/.zshrc"
+ln -sf "$DOTFILES_PATH/zsh/robbyrussell.zsh-theme" "$HOME_FOLDER/.oh-my-zsh/custom/themes/robbyrussell.zsh-theme"

de92e18a4122f4005ec5235eefe80b8bcb241d0d
Margarita Grinvald <margaritagrinvald@gmail.com>
2018-05-04 15:02:25 +0200

    Ignore future changes of bash_cutom

--- setup/setup_links.sh
+++ setup/setup_links.sh
@@ -13,3 +13,6 @@ ln -sf "$DOTFILES_PATH/bash/bashrc" "$HOME_FOLDER/.bashrc"
 # zshrc
 ln -sf "$DOTFILES_PATH/zsh/zshrc" "$HOME_FOLDER/.zshrc"
 ln -sf "$DOTFILES_PATH/zsh/robbyrussell.zsh-theme" "$HOME_FOLDER/.oh-my-zsh/custom/themes/robbyrussell.zsh-theme"
+
+# Ignore future changes of bash_custom
+cd ~/dotfiles/ && git update-index --assume-unchanged ~/dotfiles/bash/bash_custom && cd - > /dev/null
